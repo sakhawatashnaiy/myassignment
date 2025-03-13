16de548f32645752131a38698ec725b54080631e
sakhawatashnaiy <sakhawatashnaiy09@gmail.com>
2025-03-13 06:13:12 +0500

    practice

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-function changeimages(){
-     const images=   document.getElementById('images') 
+let waight = 150;
+ waight + "20";
+ console.log(waight);
+   
+//   let number = 123;
+//   alert(number)
+  var popularnumber = 4
+  console.log(popularnumber);
+  popularnumber = 2+2;
+  var num =10;
+  var popularNum = "num"+100;
+  console.log(popularNum);
 
-        const RandomCars = [
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb3zDv0dAUZn_uehbhJXghX9aAu-fAhW869A&s",
 
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQclg-pbxAwcy_N36IpE_fSp-X_Aj-wPKwiLQ&s",
+  var num = 10;
+   var anotherNum = 1;
+    var popularNumber = "num"+ anotherNum
 
 
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9-AfX-tG0wEPEp4-O_OgftAgkEUH76TrFFw&s",
+    console.log(popularNumber);
 
-           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtziPb-KLnI4OsDgMIfUtKdL2YkUwCZ3zKVg&s",
+    var whatsLeftOver = 9 % 3;
+    console.log( whatsLeftOver);
 
-           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTftsKGdZ11yndeXcnCFneLj7_e4OceKZaIIw&s",
 
-           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsZgiEB0HrCPq6VjNUtjlbdDYi5kaP8Lu1yQ&s", 
-           
-           "https://upload.wikimedia.org/wikipedia/commons/6/66/2015_Toyota_Fortuner_%28New_Zealand%29.jpg",
+    // var message = "Thanks, ";
+    //  var userName = "Susan";
+    //  var banger = "!";
+    //  var customMess
+    // = message + userName + banger;
+    //  alert(customMess
+    // )
 
-           "https://www.toyotawalton.com/wp-content/uploads/2024/10/Toyota-Corolla-X-16-SE-Toyota-Sure-Used-Vehicle-4.webp",
+    //   alert(2+4);
+       
+    // var question
+    // = "Your species?";
+    //  var defaultAnswer
+    // = "human";
+    //  var spec = prompt(question
+    // , defaultAnswer
+    // )
 
-          "https://www.gari.pk/images/new/cars/2022-03/1704_1_95765.jpg",
-          
-           
-        ]
-        
-            const car = RandomCars[Math.floor(Math.random()*RandomCars.length)]
-                images.src = car;
-              
-                  
-                //     let maxX = window.innerwidth- images.offsetWidth
-                //     let maxy = window.innerHeight - images .offsetHeight
-    
-                //     images.style .left = Math.floor(Math.random()*maxX + "px")
-                //     images.style .top = Math.floor(Math.random()* maxy  + "px")  
-    
-                //    function rotateBox(images){
-                //        images.style .transform = translatex(-50% -50%)
-                //         rotate(Math.random()*360);
-                   }
-                
-            
-                
-  
+      
